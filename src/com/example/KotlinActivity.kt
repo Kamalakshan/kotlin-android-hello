94832a6ac17446d7767143022a65c946b37605ab
kamalakshan <kamalakshantv@gmail.com>
2020-11-02 23:12:35 +0530

    This is my test commit message

--- src/com/example/KotlinActivity.kt
+++ src/com/example/KotlinActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 
 class HelloKotlin() : Activity() {
+    //test commit
     protected override fun onCreate(savedInstanceState : Bundle?) {
         super<Activity>.onCreate(savedInstanceState)
         setContentView(R.layout.main)
